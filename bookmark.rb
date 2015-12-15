0e400bbf43fe31cc3d87198b807740596d978eda
michaellennox <michaelctlennox@gmail.com>
2015-12-15 12:13:34 +0000

    Passes feature test for link submission

--- bookmark.rb
+++ bookmark.rb
@@ -8,7 +8,16 @@ class Bookmark < Sinatra::Base
 
   get '/links' do
     @links = Link.all
-    erb(:links)
+    erb :links
+  end
+
+  get '/links/new' do
+    erb :link_submission
+  end
+
+  post '/links' do
+    Link.create(title: params[:title], url: params[:url])
+    redirect '/links'
   end
 
   # start the server if ruby file executed directly
