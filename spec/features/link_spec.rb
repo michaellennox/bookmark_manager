96578a110535117278b9dba1641fbaa38c90575f
Chris Wynne <chris.wynndow@gmail.com>
2015-12-15 11:18:34 +0000

    Updated test

--- spec/features/link_spec.rb
+++ spec/features/link_spec.rb
@@ -3,7 +3,10 @@ feature 'List of links' do
   scenario 'displays a list of links' do
     Link.create(name: 'datamapper.org', url: 'https://datamapper.org/')
     visit('/')
-    expect(page).to have_content 'datamapper.org'
+    expect(page.status_code).to eq 200
+    within 'ul#links' do
+      expect(page).to have_content 'datamapper.org'
+    end
   end
 
 end
