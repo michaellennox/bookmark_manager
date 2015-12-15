2ae2c9632695c5562984a3501abc7dd27ab180fc
Jamie Brown <jamie@Jamies-MacBook-Pro.local>
2015-12-15 16:55:55 +0000

    refactor

--- app/models/link.rb
+++ app/models/link.rb
@@ -8,7 +8,7 @@ class Link
   property :title, String
 end
 
-DataMapper.setup(:default, ENV['https://boiling-cove-6084.herokuapp.com/'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
