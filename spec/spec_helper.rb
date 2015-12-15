74518c23c37d527fe2cae3df16f4f816fb5d4bfc
Chris Wynne <chris.wynndow@gmail.com>
2015-12-15 15:55:47 +0000

    Adds heroku database

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -53,7 +53,6 @@ RSpec.configure do |config|
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
-
   end
 
   config.before(:suite) do
@@ -68,7 +67,8 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
-  
+
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
