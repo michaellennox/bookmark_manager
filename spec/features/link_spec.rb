625b7e653a13be5065fa9504ecfa0516a3ce2b63
Chris Wynne <chris.wynndow@gmail.com>
2015-12-15 11:56:33 +0000

    Passing first features test

--- spec/features/link_spec.rb
+++ spec/features/link_spec.rb
@@ -2,7 +2,7 @@ feature 'List of links' do
 
   scenario 'displays a list of links' do
     Link.create(title: 'datamapper.org', url: 'https://datamapper.org/')
-    visit('/')
+    visit('/links')
     expect(page.status_code).to eq 200
     within 'ul#links' do
       expect(page).to have_content 'datamapper.org'
