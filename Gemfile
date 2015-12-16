cf556839f03220abfe197cd337a59faf89b98a62
Daniela <daniela.galeote@gmail.com>
2015-12-16 12:13:35 +0000

    heroku installed

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ group :test do
   gem 'data_mapper'
   gem 'dm-postgres-adapter'
   gem 'rspec-sinatra'
+  gem 'rspec'
   gem 'capybara'
   gem 'database_cleaner'
+  gem 'pg'
 end
--- app/models/links.rb
+++ app/models/links.rb
@@ -20,6 +20,7 @@ class Link
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+# DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/bookmarkdaniela')
 DataMapper.finalize
 DataMapper.auto_upgrade!
