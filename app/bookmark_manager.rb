aaeb20fed6a965929e5199755c38e2fd9242a704
michaellennox <michaelctlennox@gmail.com>
2016-01-01 19:48:51 +0000

    Adds feature for users being able to filter links by tag

--- app/bookmark_manager.rb
+++ app/bookmark_manager.rb
@@ -15,9 +15,15 @@ class BookmarkManager < Sinatra::Base
 
   post '/links' do
     link = Link.new(title: params[:title], url: params[:url])
-    tag = Tag.create(name: params[:tags])
+    tag = Tag.first_or_create(name: params[:tags])
     link.tags << tag
     link.save
     redirect to('/links')
+  end
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
   end
 end
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -1,9 +1,22 @@
 feature 'Viewing Links' do
+
+  before(:each) do
+    Link.create(url: 'http://www.michaellennox.me/', title: 'My Site\'s Home', tags: [Tag.first_or_create(name: 'home')])
+    Link.create(url: 'http://www.michaellennox.me/blog', title: 'My Site\'s Blog', tags: [Tag.first_or_create(name: 'blog')])
+  end
+
   scenario 'User can see existing links on links page' do
-    Link.create(url: 'http://www.michaellennox.me/', title: 'My Site')
     visit '/links'
     within 'ul#links' do
       expect(page).to have_content 'My Site'
+    end
+  end
+
+  scenario 'User can filter links by tags' do
+    visit '/tags/blog'
+    within 'ul#links' do
+      expect(page).to have_content 'My Site\'s Blog'
+      expect(page).not_to have_content 'My Site\'s Home'
     end
   end
 end
